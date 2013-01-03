de4181348f5780a906ddc6e2b5c2d647b01d2969
Bajee11 <bajee11@gmail.com>
2013-01-02 23:53:10 -0500

    Buildscript for AOKP

--- build_all.sh
+++ build_all.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
 ydate=$(date -d '1 day ago' +"%m/%d/%Y")
-sdate="$2"
+sdate="$1"
 cdate=`date +"%m_%d_%Y"`
 rdir=`pwd`
-RELEASE="$1"
 
 
-# Build RootBox SGH-I747
+# Build AOKP SGH-I747
 make clobber;
 . build/envsetup.sh;
-brunch rootbox_d2att-userdebug;
+brunch aokp_d2att-userdebug;
 
 # Get Package Name
-sed -i -e 's/rootbox_//' $OUT/system/build.prop
-VERSION1=`sed -n -e'/ro.rootbox.version/s/^.*=//p' $OUT/system/build.prop`
+VERSION1=`sed -n -e'/ro.aokp.version/s/^.*=//p' $OUT/system/build.prop`
 PACKAGEd2att=$OUT/$VERSION1.zip
 
 # Generate Changelog
@@ -45,7 +43,9 @@ do
         elif [ "$origin" = "faux" ]; then
             proj_credit=Faux123
         elif [ "$origin" = "rootbox" ]; then
-            proj_credit=RootBox
+            proj_credit=Bajee11
+        elif [ "$origin" = "bajee" ]; then
+            proj_credit=Baje11
         else
             proj_credit=""
         fi
@@ -59,101 +59,46 @@ do
     fi
 done
 
-# Create Version Changelog
-if [ "$RELEASE" == "nightly" ]
-then
-    echo "Not generating version changelog for nightlies"
-else
-    cp changelog.txt changelog_"$RB_BUILD".txt
-fi
+# Make changelog copy
+cp changelog.txt Changelog_"$cdate".txt
 
-# Move the changelog into d2att zip  & upload zip/changelog to Goo.im
-
-if [ "$RELEASE" == "nightly" ]
-then
-    find "$OUT" -name *RootBox-JB-*${VERSION}*.zip -exec zip -j {} "$rdir"/changelog.txt \;
-    scp "$PACKAGEd2att" Bajee@upload.goo.im:~/public_html/Nightlies/d2att
-    scp "$PACKAGEd2att" bajee11@exynos.co:~/RB_d2att_NIGHTLIES
-else
-    find "$rdir"/out/target/product -name *RootBox-JB-*${RB_BUILD}*.zip -exec zip -j {} "$rdir"/changelog.txt \;
-    scp "$PACKAGEd2att" Bajee@upload.goo.im:~/public_html/RootBox_d2att_jb
-    scp "$rdir"/changelog_"$RB_BUILD".txt Bajee@upload.goo.im:~/public_html/RootBox_Changelogs
-fi
+# Upload zip/changelog to Andro
+find "$OUT" -name *aokp_d2att_unofficial*.zip -exec zip -j {} "$rdir"/changelog.txt \;
+scp "$rdir"/Changelog_$cdate.txt bajee11@exynos.co:~/AOKP_Changelog
+scp "$PACKAGEd2att" bajee11@exynos.co:~/AOKP_d2att_NIGHTLIES
 
 
-# Build RootBox GT-I9100
+# Build AOKP GT-I9100
 make clobber;
 . build/envsetup.sh;
-brunch rootbox_i9100-userdebug;
+brunch aokp_i9100-userdebug;
 
 # Get Package Name
-sed -i -e 's/rootbox_//' $OUT/system/build.prop
-VERSION2=`sed -n -e'/ro.rootbox.version/s/^.*=//p' $OUT/system/build.prop`
+find "$OUT" -name *aokp_i9100_unofficial*.zip -exec zip -j {} "$rdir"/changelog.txt \;
+VERSION2=`sed -n -e'/ro.aokp.version/s/^.*=//p' $OUT/system/build.prop`
 PACKAGEi9100=$OUT/$VERSION2.zip
 
-# Move the changelog into zip  & upload zip to Goo.im
-if [ "$RELEASE" == "nightly" ]
-then
-    find "$OUT" -name *RootBox-JB-*${VERSION}*.zip -exec zip -j {} "$rdir"/changelog.txt \;
-    scp "$PACKAGEi9100" Bajee@upload.goo.im:~/public_html/Nightlies/i9100
-    scp "$PACKAGEi9100" bajee11@exynos.co:~/RB_i9100_NIGHTLIES
-else
-    find "$OUT" -name *RootBox-JB-*${RB_BUILD}*.zip -exec zip -j {} "$rdir"/changelog.txt \;
-    scp "$PACKAGEi9100" Bajee@upload.goo.im:~/public_html/RootBox_i9100_jb
-fi
+# Upload package to Andro
+scp "$PACKAGEi9100" bajee11@exynos.co:~/AOKP_i9100_NIGHTLIES
 
 # Build RootBox GT-I9300
 make clobber;
 . build/envsetup.sh;
-brunch rootbox_i9300-userdebug;
+brunch aokp_i9300-userdebug;
 
 # Get Package Name
-sed -i -e 's/rootbox_//' $OUT/system/build.prop
-VERSION4=`sed -n -e'/ro.rootbox.version/s/^.*=//p' $OUT/system/build.prop`
+find "$OUT" -name *aokp_i9300_unofficial*.zip -exec zip -j {} "$rdir"/changelog.txt \;
+VERSION4=`sed -n -e'/ro.aokp.version/s/^.*=//p' $OUT/system/build.prop`
 PACKAGEi9300=$OUT/$VERSION4.zip
 
-# Move the changelog into zip  & upload zip to Goo.im
-if [ "$RELEASE" == "nightly" ]
-then
-    find "$OUT" -name *RootBox-JB-*${VERSION}*.zip -exec zip -j {} "$rdir"/changelog.txt \;
-    scp "$PACKAGEi9300" Bajee@upload.goo.im:~/public_html/Nightlies/i9300
-    scp "$PACKAGEi9300" bajee11@exynos.co:~/RB_i9300_NIGHTLIES
-else
-    find "$OUT" -name *RootBox-JB-*${RB_BUILD}*.zip -exec zip -j {} "$rdir"/changelog.txt \;
-    scp "$PACKAGEi9300" Bajee@upload.goo.im:~/public_html/RootBox_i9300_jb
-fi
-
-# Build RootBox GT-I9100P
-#make clobber;
-#. build/envsetup.sh;
-#brunch rootbox_i9100p-userdebug;
-
-# Get Package Name
-#sed -i -e 's/rootbox_//' $OUT/system/build.prop
-#VERSION3=`sed -n -e'/ro.rootbox.version/s/^.*=//p' $OUT/system/build.prop`
-#PACKAGEi9100p=$OUT/$VERSION3.zip
-#
-# Move the changelog into zip  & upload zip to Goo.im
-#if [ "$RELEASE" == "nightly" ]
-#then
-#    find "$OUT" -name *RootBox-JB-*${VERSION}*.zip -exec zip -j {} "$rdir"/changelog.txt \;
-#    scp "$PACKAGEi9100p" Bajee@upload.goo.im:~/public_html/Nightlies/i9100p
-#    scp "$PACKAGEi9100p" bajee11@exynos.co:~/RB_i9100p_NIGHTLIES
-#else
-#    find "$OUT" -name *RootBox-JB-*${RB_BUILD}*.zip -exec zip -j {} "$rdir"/changelog.txt \;
-#    scp "$PACKAGEi9100p" Bajee@upload.goo.im:~/public_html/RootBox_i9100p_jb
-#fi
+# Upload package to Andro
+scp "$PACKAGEi9300" bajee11@exynos.co:~/AOKP_i9300_NIGHTLIES
 
 # Remove Changelogs
-if [ "$RELEASE" == "nightly" ]
-then
-    rm "$rdir"/changelog.txt
-else
-    rm "$rdir"/changelog.txt
-    rm "$rdir"/changelog_"$RB_BUILD".txt
-fi
+rm "$rdir"/changelog.txt
+rm "$rdir"/Changelog_$cdate.txt
 
-echo "RootBox packages built, Changelog generated and everything uploaded to server!"
+echo "AOKP packages built, Changelog generated and everything uploaded to server!"
 
 
 exit 0
