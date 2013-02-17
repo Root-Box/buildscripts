29cd19009295c1dd10dc7914e8cadd9d61e885c0
Bajee11 <bajee11@gmail.com>
2013-02-17 00:31:37 -0500

    Cleanup Buildscript and build Nexus devices first.

--- build_all.sh
+++ build_all.sh
@@ -27,14 +27,7 @@ else
     echo "Building Nightly"
 fi
 
-# Build RootBox SGH-I747
-. build/envsetup.sh;
-brunch rootbox_d2att-userdebug;
-
-# Get Package Name
-sed -i -e 's/rootbox_//' $OUT/system/build.prop
-VERSION1=`sed -n -e'/ro.rootbox.version/s/^.*=//p' $OUT/system/build.prop`
-PACKAGEd2att=$OUT/$VERSION1.zip
+echo "Generating Changelog"
 
 # Generate Changelog
 
@@ -63,6 +56,8 @@ do
             proj_credit=CyanogenMod
         elif [ "$origin" = "faux" ]; then
             proj_credit=Faux123
+        elif [ "$origin" = "bajee" ]; then
+            proj_credit=RootBox
         elif [ "$origin" = "rootbox" ]; then
             proj_credit=RootBox
         else
@@ -81,24 +76,70 @@ done
 # Create Version Changelog
 if [ "$RELEASE" == "nightly" ]
 then
-    echo "Generating Changelog for Nightly"
+    echo "Generating and Uploading Changelog for Nightly"
     cp changelog.txt changelog_"$DATE".txt
+    scp "$rdir"/changelog_"$DATE".txt Bajee@upload.goo.im:~/public_html/Nightlies/Changelogs
 else
-    echo "Generating Changelog for Official Release"
+    echo "Generating and Uploading Changelog for Official Release"
     cp changelog.txt changelog_"$RB_BUILD".txt
+    scp "$rdir"/changelog_"$RB_BUILD".txt Bajee@upload.goo.im:~/public_html/RootBox_Changelogs
 fi
 
-# Move the changelog into d2att zip  & upload zip/changelog to Goo.im
+# Build RootBox Mako (Nexus 4)
+. build/envsetup.sh;
+brunch rootbox_mako-userdebug;
+
+# Get Package Name
+sed -i -e 's/rootbox_//' $OUT/system/build.prop
+VERSION1=`sed -n -e'/ro.rootbox.version/s/^.*=//p' $OUT/system/build.prop`
+PACKAGEmako=$OUT/$VERSION1.zip
+
+# Move the changelog into zip  & upload zip to Goo.im
+if [ "$RELEASE" == "nightly" ]
+then
+    find "$OUT" -name *RootBox-JB-mako-Nightly-*${DATE}*.zip -exec zip -j {} "$rdir"/changelog.txt \;
+    scp "$PACKAGEmako" Bajee@upload.goo.im:~/public_html/Nightlies/mako
+else
+    find "$OUT" -name *RootBox-JB-mako-*${RB_BUILD}*.zip -exec zip -j {} "$rdir"/changelog.txt \;
+    scp "$PACKAGEmako" Bajee@upload.goo.im:~/public_html/RootBox_mako_jb
+fi
+
+
+# Build RootBox Grouper
+brunch rootbox_grouper-userdebug;
+
+# Get Package Name
+sed -i -e 's/rootbox_//' $OUT/system/build.prop
+VERSION2=`sed -n -e'/ro.rootbox.version/s/^.*=//p' $OUT/system/build.prop`
+PACKAGEgrouper=$OUT/$VERSION2.zip
+
+# Move the changelog into zip  & upload zip to Goo.im
+if [ "$RELEASE" == "nightly" ]
+then
+    find "$OUT" -name *RootBox-JB-grouper-Nightly-*${DATE}*.zip -exec zip -j {} "$rdir"/changelog.txt \;
+    scp "$PACKAGEgrouper" Bajee@upload.goo.im:~/public_html/Nightlies/grouper
+else
+    find "$OUT" -name *RootBox-JB-grouper-*${RB_BUILD}*.zip -exec zip -j {} "$rdir"/changelog.txt \;
+    scp "$PACKAGEgrouper" Bajee@upload.goo.im:~/public_html/RootBox_grouper_jb
+fi
+
+# Build RootBox SGH-I747
+brunch rootbox_d2att-userdebug;
+
+# Get Package Name
+sed -i -e 's/rootbox_//' $OUT/system/build.prop
+VERSION3=`sed -n -e'/ro.rootbox.version/s/^.*=//p' $OUT/system/build.prop`
+PACKAGEd2att=$OUT/$VERSION3.zip
+
+# Move the changelog into zip  & upload zip to Goo.im
 
 if [ "$RELEASE" == "nightly" ]
 then
     find "$OUT" -name *RootBox-JB-d2att-Nightly-*${DATE}*.zip -exec zip -j {} "$rdir"/changelog.txt \;
     scp "$PACKAGEd2att" Bajee@upload.goo.im:~/public_html/Nightlies/d2att
-    scp "$rdir"/changelog_"$DATE".txt Bajee@upload.goo.im:~/public_html/Nightlies/Changelogs
 else
     find "$rdir"/out/target/product -name *RootBox-JB-d2att-*${RB_BUILD}*.zip -exec zip -j {} "$rdir"/changelog.txt \;
     scp "$PACKAGEd2att" Bajee@upload.goo.im:~/public_html/RootBox_d2att_jb
-    scp "$rdir"/changelog_"$RB_BUILD".txt Bajee@upload.goo.im:~/public_html/RootBox_Changelogs
 fi
 
 # Build RootBox SGH-T999
@@ -106,8 +147,8 @@ brunch rootbox_d2tmo-userdebug;
 
 # Get Package Name
 sed -i -e 's/rootbox_//' $OUT/system/build.prop
-VERSION2=`sed -n -e'/ro.rootbox.version/s/^.*=//p' $OUT/system/build.prop`
-PACKAGEd2tmo=$OUT/$VERSION2.zip
+VERSION4=`sed -n -e'/ro.rootbox.version/s/^.*=//p' $OUT/system/build.prop`
+PACKAGEd2tmo=$OUT/$VERSION4.zip
 
 # Move the changelog into zip  & upload zip to Goo.im
 if [ "$RELEASE" == "nightly" ]
@@ -124,8 +165,8 @@ brunch rootbox_d2vzw-userdebug;
 
 # Get Package Name
 sed -i -e 's/rootbox_//' $OUT/system/build.prop
-VERSION8=`sed -n -e'/ro.rootbox.version/s/^.*=//p' $OUT/system/build.prop`
-PACKAGEd2vzw=$OUT/$VERSION8.zip
+VERSION5=`sed -n -e'/ro.rootbox.version/s/^.*=//p' $OUT/system/build.prop`
+PACKAGEd2vzw=$OUT/$VERSION5.zip
 
 # Move the changelog into zip  & upload zip to Goo.im
 if [ "$RELEASE" == "nightly" ]
@@ -142,8 +183,8 @@ brunch rootbox_i9100-userdebug;
 
 # Get Package Name
 sed -i -e 's/rootbox_//' $OUT/system/build.prop
-VERSION3=`sed -n -e'/ro.rootbox.version/s/^.*=//p' $OUT/system/build.prop`
-PACKAGEi9100=$OUT/$VERSION3.zip
+VERSION6=`sed -n -e'/ro.rootbox.version/s/^.*=//p' $OUT/system/build.prop`
+PACKAGEi9100=$OUT/$VERSION6.zip
 
 # Move the changelog into zip  & upload zip to Goo.im
 if [ "$RELEASE" == "nightly" ]
@@ -160,8 +201,8 @@ brunch rootbox_i9100g-userdebug;
 
 # Get Package Name
 sed -i -e 's/rootbox_//' $OUT/system/build.prop
-VERSION4=`sed -n -e'/ro.rootbox.version/s/^.*=//p' $OUT/system/build.prop`
-PACKAGEi9100g=$OUT/$VERSION4.zip
+VERSION7=`sed -n -e'/ro.rootbox.version/s/^.*=//p' $OUT/system/build.prop`
+PACKAGEi9100g=$OUT/$VERSION7.zip
 
 # Move the changelog into zip  & upload zip to Goo.im
 if [ "$RELEASE" == "nightly" ]
@@ -178,8 +219,8 @@ brunch rootbox_i9300-userdebug;
 
 # Get Package Name
 sed -i -e 's/rootbox_//' $OUT/system/build.prop
-VERSION5=`sed -n -e'/ro.rootbox.version/s/^.*=//p' $OUT/system/build.prop`
-PACKAGEi9300=$OUT/$VERSION5.zip
+VERSION8=`sed -n -e'/ro.rootbox.version/s/^.*=//p' $OUT/system/build.prop`
+PACKAGEi9300=$OUT/$VERSION8.zip
 
 # Move the changelog into zip  & upload zip to Goo.im
 if [ "$RELEASE" == "nightly" ]
@@ -191,47 +232,11 @@ else
     scp "$PACKAGEi9300" Bajee@upload.goo.im:~/public_html/RootBox_i9300_jb
 fi
 
-# Build RootBox Grouper
-brunch rootbox_grouper-userdebug;
-
-# Get Package Name
-sed -i -e 's/rootbox_//' $OUT/system/build.prop
-VERSION6=`sed -n -e'/ro.rootbox.version/s/^.*=//p' $OUT/system/build.prop`
-PACKAGEgrouper=$OUT/$VERSION6.zip
-
-# Move the changelog into zip  & upload zip to Goo.im
-if [ "$RELEASE" == "nightly" ]
-then
-    find "$OUT" -name *RootBox-JB-grouper-Nightly-*${DATE}*.zip -exec zip -j {} "$rdir"/changelog.txt \;
-    scp "$PACKAGEgrouper" Bajee@upload.goo.im:~/public_html/Nightlies/grouper
-else
-    find "$OUT" -name *RootBox-JB-grouper-*${RB_BUILD}*.zip -exec zip -j {} "$rdir"/changelog.txt \;
-    scp "$PACKAGEgrouper" Bajee@upload.goo.im:~/public_html/RootBox_grouper_jb
-fi
-
-# Build RootBox Mako (Nexus 4)
-brunch rootbox_mako-userdebug;
-
-# Get Package Name
-sed -i -e 's/rootbox_//' $OUT/system/build.prop
-VERSION7=`sed -n -e'/ro.rootbox.version/s/^.*=//p' $OUT/system/build.prop`
-PACKAGEmako=$OUT/$VERSION7.zip
-
-# Move the changelog into zip  & upload zip to Goo.im
-if [ "$RELEASE" == "nightly" ]
-then
-    find "$OUT" -name *RootBox-JB-mako-Nightly-*${DATE}*.zip -exec zip -j {} "$rdir"/changelog.txt \;
-    scp "$PACKAGEmako" Bajee@upload.goo.im:~/public_html/Nightlies/mako
-else
-    find "$OUT" -name *RootBox-JB-mako-*${RB_BUILD}*.zip -exec zip -j {} "$rdir"/changelog.txt \;
-    scp "$PACKAGEmako" Bajee@upload.goo.im:~/public_html/RootBox_mako_jb
-fi
-
-
 # Remove Changelogs
 if [ "$RELEASE" == "nightly" ]
 then
     rm "$rdir"/changelog.txt
+    rm "$rdir"/changelog_"$DATE".txt
 else
     rm "$rdir"/changelog.txt
     rm "$rdir"/changelog_"$RB_BUILD".txt
